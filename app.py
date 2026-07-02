af93cd1bf130cf32e295766a424243010f748fd7
jethame-ng <jethame.ng@student.mmu.edu.my>
2026-07-02 16:00:38 +0800

    edit wording, 2/7/2026, 4:00pm

--- app.py
+++ app.py
@@ -27,27 +27,27 @@ st.markdown("""
 
 st.info("Use the sidebar to navigate between pages.")
 
-st.subheader("Project Files")
+# st.subheader("Project Files")
 
-total_size = 0
+# total_size = 0
 
-for folder in ["data", "model"]:
-    if os.path.exists(folder):
-        st.write(f"### {folder}")
+# for folder in ["data", "model"]:
+#     if os.path.exists(folder):
+#         st.write(f"### {folder}")
 
-        for file in os.listdir(folder):
-            path = os.path.join(folder, file)
+#         for file in os.listdir(folder):
+#             path = os.path.join(folder, file)
 
-            if os.path.isfile(path):
-                size = os.path.getsize(path) / (1024 * 1024)
-                total_size += os.path.getsize(path)
+#             if os.path.isfile(path):
+#                 size = os.path.getsize(path) / (1024 * 1024)
+#                 total_size += os.path.getsize(path)
 
-                st.write(f"{file} : {size:.2f} MB")
+#                 st.write(f"{file} : {size:.2f} MB")
 
-st.success(f"Total Project Size: {total_size/(1024*1024):.2f} MB")
+# st.success(f"Total Project Size: {total_size/(1024*1024):.2f} MB")
 
-total, used, free = shutil.disk_usage("/")
+# total, used, free = shutil.disk_usage("/")
 
-st.write(f"Total Disk : {total // (1024**3)} GB")
-st.write(f"Used Disk  : {used // (1024**3)} GB")
-st.write(f"Free Disk  : {free // (1024**3)} GB")
+# st.write(f"Total Disk : {total // (1024**3)} GB")
+# st.write(f"Used Disk  : {used // (1024**3)} GB")
+# st.write(f"Free Disk  : {free // (1024**3)} GB")
